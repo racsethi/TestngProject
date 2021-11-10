d5f211df079cd692e53a02628538352b8d9ccd78
rachit <sethirachit1996@gmail.com>
2021-11-10 07:24:46 +0000

    TestngProject

--- src/test/Baseclass.java
+++ src/test/Baseclass.java
@@ -1,16 +1,10 @@
-package test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-public class Baseclass {
-
-	
- public static WebDriver driver;
+package test; 
+ 
+ @BeforeTest
+ 
+ public void ReportSetup() throws IOException{
+	 report = new ExtentReports("ExtentReport.html");
+ }
 
 @BeforeMethod
 public void setup() {
@@ -27,5 +21,14 @@ public void setup() {
 public void teardown()  {
 	
 	driver.quit();
+}
+
+@AfterTest
+public void ReportTearDowm() throws IOException {
+	wbook.close();
+	report.flush();
+	report.close();
+	
+	
 }
 }
--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -7,6 +7,8 @@ import org.testng.asserts.SoftAssert;
 
 import pages.LoginPage;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -16,6 +18,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class LoginTest extends Baseclass {
+	
+	XSSFWorkbook wbook;
+    XSSFSheet sheet;
 	 @Parameters({"UserName","Password"}) 
      @Test
      public void NegativeLogin(String UserNameVal,String PasswordVal) {
@@ -27,16 +32,32 @@ public class LoginTest extends Baseclass {
     }
 	 
 	 @Parameters({"UserName","CorrectPassword"}) 
-     @Test
+     @Test(enabled = false)
      public void  PositiveLogin(String UserNameVal,String PasswordVal) {
 		 
 		 LoginPage login = new LoginPage();
 		login. Login(UserNameVal,PasswordVal);	 
-  
+         report.endTest(test);
    
     }
 	 
+	 @Test
+	 public void  ExcelTest() throws IOException{
+		 FileInputStream fis = new  FileInputStream("exceldata.xlsx");
+		 wbook = new XSSFWorkbook(fis);
+		 sheet = wbok.getSheet("data");
+	     String UserNameVal = sheet.getRow(0).getCell(0).getStringCellValue();
+	     String PasswordVal = sheet.getRow(0).getCell(1).getStringCellValue();
+	     test = report.startTest("Negative Login Test");
+	     LoginPage login = new LoginPage();
+			login. Login(UserNameVal,PasswordVal);	 
+	   report.endTest(test);
+	 }
+
 	 
+	 
+	 
+	
 	 
 	}
   
